f076fada17f02c099c4bcab34845266ea11d98af
Lukasz <lukasz.gras.97@gmail.com>
2020-09-28 17:27:35 +0200

    Basic config

---
+++ src/components/main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Index = () => {
+    return (
+        <div>
+            <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, vero?</h1>
+            <button>Add</button>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cum cupiditate delectus dolor dolores ea expedita ipsam laborum magnam minus nam neque quas quasi quia reprehenderit, sint suscipit voluptates voluptatum. Aperiam assumenda at aut culpa delectus ea explicabo harum id iusto laboriosam minima mollitia necessitatibus neque nobis placeat porro quis sed, unde. A adipisci asperiores at consequuntur cupiditate delectus dignissimos dolores, enim expedita modi numquam perspiciatis quibusdam reprehenderit sit sunt veritatis voluptate voluptates. At blanditiis cumque, deserunt ea enim error expedita facilis fuga harum in inventore itaque laborum maxime modi natus nemo porro, quibusdam quo quos voluptatibus! Aliquid, corporis, repellat.</p>
+        </div>
+    );
+};
+
+export default Index;
